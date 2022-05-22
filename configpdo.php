deed1c8c4a3e4284edf1e35b8b35f03bf3cd335c
MFITUMUKIZA Aminadab <105486509+MfiitumukizaAminadab@users.noreply.github.com>
2022-05-22 20:00:58 +0200

    Add files via upload

---
+++ configpdo.php
@@ -0,0 +1,8 @@
+<?php
+$dbuser="root";
+$dbpass="";
+$host="localhost";
+$dbname = "tumba";
+$mysqli = new mysqli($host, $dbuser, $dbpass, $dbname);
+
+?>
---
+++ topbar.php
@@ -0,0 +1,32 @@
+  <nav class="navbar top-navbar bg-white box-shadow">
+            	<div class="container-fluid">
+                    <div class="row">
+                        <div class="navbar-header no-padding">
+                			<a class="navbar-brand" href="dashboard.php">
+                			    Tumba  Result System            
+                			</a>
+                            
+                            <button type="button" class="navbar-toggle mobile-nav-toggle" >
+                				<i class="fa fa-bars"></i>
+                			</button>
+                		</div>
+                       
+
+                            <!-- /.nav navbar-nav -->
+
+                			<ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                             
+                				
+                				    <li><a href="logout.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Logout</a></li>
+                					
+                		
+                            
+                			</ul>
+                            <!-- /.nav navbar-nav navbar-right -->
+                		</div>
+                		<!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.row -->
+            	</div>
+            	<!-- /.container-fluid -->
+            </nav>
